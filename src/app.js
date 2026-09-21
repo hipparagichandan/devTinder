12ffbe14835d5f90ec875fca40b982e51465cb8e
hipparagichandan <113527043+hipparagichandan@users.noreply.github.com>
2026-09-21 16:18:20 +0530

    Playing with routes - regex, params & queries

--- src/app.js
+++ src/app.js
@@ -3,6 +3,12 @@ const express = require('express')
 const app = express()
 
 //Order of the routes matter, the first matching wildcard for that route will be resolved
+app.get("/user/:userId" , (req,res) =>{
+    console.log("Req params ",req.params)
+    console.log("Req queries ",req.query)
+    res.send("You have requested GET for userID : " + req.params.userId + " and optional queries : " + JSON.stringify(req.query) )
+})
+
 app.get("/user" , (req, res)=> {
     res.send({"firstname" : "Chandan", "secondname" : "Hipparagi"})
 })
@@ -27,6 +33,10 @@ app.delete("/user", (req,res)=>{
 //this will match all the routes that starts with /test eg: /test/123 will be resolved here
 app.use("/test",(req, res)=>{
     res.send("Hello from test server ")
+})
+
+app.get('{*splat}', (req, res)=>{
+    res.send("Please check your routes")
 })
 
 app.listen(7777, ()=>{

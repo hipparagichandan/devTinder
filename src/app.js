b6f5b557f5db4c068a2ed4ccae870c329242233a
hipparagichandan <113527043+hipparagichandan@users.noreply.github.com>
2026-09-21 20:07:37 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,38 +2,23 @@ const express = require('express')
 
 const app = express()
 
-//Order of the routes matter, the first matching wildcard for that route will be resolved
-app.get("/user/:userId" , (req,res) =>{
-    console.log("Req params ",req.params)
-    console.log("Req queries ",req.query)
-    res.send("You have requested GET for userID : " + req.params.userId + " and optional queries : " + JSON.stringify(req.query) )
-})
-
-app.get("/user" , (req, res)=> {
-    res.send({"firstname" : "Chandan", "secondname" : "Hipparagi"})
-})
-
-app.post("/user", (req,res) => {
-    // handle post logic
-    res.send("User Post succesful")
-})
-
-app.put("/user", (_,res)=> {
-    res.send("Updated User sucesfully")
-})
-
-app.patch("/user", (req, res)=> {
-    res.send("Patch request successful")
-})
-
-app.delete("/user", (req,res)=>{
-    res.send("Deleted Successfully")
-})
-
-//this will match all the routes that starts with /test eg: /test/123 will be resolved here
-app.use("/test",(req, res)=>{
-    res.send("Hello from test server ")
-})
+app.use(
+    "/user", 
+    [(req, res, next) => {
+        console.log("Handling Route user 1")
+        next();
+        // res.send(" 1st Resonse")
+    },
+    (req, res, next) => {
+        console.log("handling route user 2")
+        // res.send(" 2nd Response")
+        next()
+    }],
+    (req, res) => {
+        console.log("handling Response 3")
+        res.send(" 3rd Response")
+    }
+)
 
 app.get('{*splat}', (req, res)=>{
     res.send("Please check your routes")
